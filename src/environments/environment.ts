d1a3b4cfbcf419003a440228c0b0782ad0d4988c
Rei Armenia <lazerzes@gmail.com>
2020-03-13 08:05:36 -0400

    dependicies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBbJ1ismjgj3JyJla8oCiaxX_vvTJO8gD8',
+    authDomain: 'lazerzes-db.firebaseapp.com',
+    databaseURL: 'https://lazerzes-db.firebaseio.com',
+    projectId: 'lazerzes-db',
+    storageBucket: 'lazerzes-db.appspot.com',
+    messagingSenderId: '319630326777',
+    appId: '1:319630326777:web:10735425ee7ed6a3f39b28',
+    measurementId: 'G-5DT7Q1NHQJ'
+  }
 };
 
 /*
